638a6baa37670ab9df9905c88907fde57a30a0b7
RedSkull_7_7 <icaro.nata147@gmail.com>
2023-09-26 07:58:17 -0300

    Estilizando lista de tarefas

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,7 +1,27 @@
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+
 const ListaDeTarefas = () => (
-  <main>
+  <Container>
     <p>2 Tarefas marcada como &quot;Categoria e &quot;termo</p>
-  </main>
+    <ul>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+    </ul>
+  </Container>
 )
 
 export default ListaDeTarefas
