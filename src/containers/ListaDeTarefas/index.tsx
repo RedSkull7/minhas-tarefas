3b2d00293dfe3af2a479263a85ecf0584959e601
RedSkull_7_7 <icaro.nata147@gmail.com>
2023-09-26 08:23:33 -0300

    Componetizando e add atributos aos eleme. tarefas

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,25 +1,41 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver a aula 3 da EBAC',
+    prioridade: 'Importante',
+    status: 'Pendente'
+  },
+  {
+    titulo: 'Pagar conta da internet',
+    descricao: 'Baixar fatura no gmail',
+    prioridade: 'Urgente',
+    status: 'Concluída'
+  },
+  {
+    titulo: 'Ir para academia',
+    descricao: 'Fazer treino de perna',
+    prioridade: 'Importante',
+    status: 'Pendente'
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <p>2 Tarefas marcada como &quot;Categoria e &quot;termo</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
